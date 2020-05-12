4b08747efc1407e87456c9b6827fcf53f039496f
bokesyo <63912729+bokesyo@users.noreply.github.com>
2020-05-12 11:06:01 +0800

    画图功能完善 导出脚本功能完善 整理干净

--- classRef/localCache.py
+++ classRef/localCache.py
@@ -7,18 +7,9 @@ class local_cache:
         self.cache = cache
 
     def __setitem__(self, key, value):
-        """
-        key: 变量名
-        value: 变量值
-        cache: 缓存名
-        """
         with closing(shelve.open(self.cache, 'c')) as shelf:
             shelf[key] = value
 
     def __getitem__(self, key):
-        """
-        key : 变量名
-        return：变量值
-        """
         with closing(shelve.open(self.cache, 'r')) as shelf:
             return shelf[key]
