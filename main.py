9b4d952afa059c675e6f8bd376b5a72f53cdc160
bokesyo <63912729+bokesyo@users.noreply.github.com>
2020-05-13 08:59:57 +0800

    正式版 Ver6

---
+++ main.py
@@ -0,0 +1,5 @@
+print('Firstly, we generate a primary tree, it takes several seconds...')
+print('Computing...')
+from primaryGrow import *
+print('Finished, a primary tree has been generated.')
+print(tree)
