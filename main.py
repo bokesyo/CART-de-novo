26278237727b7026b595464aee8a36ae50aec04b
bokesyo <63912729+bokesyo@users.noreply.github.com>
2020-05-15 18:24:34 +0800

    正式版 Ver7 回归树和分类树均有

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from regGrow import *
 from regAsses import *
+from regPrune import *
+from readData import *
 
+data_dict = readCSV('inputData/partial_train.csv')
+
+# Growing
 T = regGrow('inputData/partial_train.csv', 'tmp/reg/treeObj')
 tree = T.tree
 
-A = regAsses('inputData/test.csv', tree)
 
-print('MSE:', A.mse, 'MAE:', A.mae, 'Accuracy(+-1):', A.rate)
+# Pruning
+stop = False
+n = 1
+
+while tree.size > 1:
+    this = regPrune(tree, data_dict)
+    tree = this.tree
+    data = local_cache('tmp/reg/forest/' + str(n))
+    data['tree'] = tree
+    n = n + 1
 
 
-
+# Filtering
+m = 1
+for i in range(1, n):
+    data = local_cache('tmp/reg/forest/' + str(m))
+    tree = data['tree']
+    A = regAsses('inputData/remain_train.csv', tree)
+    print(m, ',', A.mse, ',', A.mae, ',', A.rate)
+    m = m + 1
